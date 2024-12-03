1d6f095a4039ee223554395780309b9e5521c18c
aabidshaikh001 <technicalsheikhshabb@gmail.com>
2024-12-03 12:50:42 +0530

    okay

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,34 +6,30 @@ import AppRoutes from "./approutes";
 import ScrollToTop from "./scrolltop";
 import "./globals.css";
 
-
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout() {
   return (
     <html lang="en">
-     
       <head>
-    <title>TapirSoft: Your Trusted B2B Software Solutions Provider</title>
-    <meta name="description" content="TapirSoft provides cutting-edge B2B software solutions tailored to streamline your business operations and drive growth. Discover how we can empower your business." />
-    <meta name="keywords" content="TapirSoft, B2B software solutions, business software, enterprise solutions, software provider, business growth, streamline operations" />
-    <meta name="author" content="TapirSoft" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" href="/assests/TAPIR.jpg" type="image/x-icon" />
-    <link rel="shortcut icon" href="/assests/TAPIR.jpg" type="image/x-icon" />
-</head>
-
-        
+        <title>TapirSoft: Your Trusted B2B Software Solutions Provider</title>
+        <meta
+          name="description"
+          content="TapirSoft provides cutting-edge B2B software solutions tailored to streamline your business operations and drive growth. Discover how we can empower your business."
+        />
+        <meta
+          name="keywords"
+          content="TapirSoft, B2B software solutions, business software, enterprise solutions, software provider, business growth, streamline operations"
+        />
+        <meta name="author" content="TapirSoft" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/assests/TAPIR.jpg" type="image/x-icon" />
+        <link rel="shortcut icon" href="/assests/TAPIR.jpg" type="image/x-icon" />
+      </head>
       <body>
-         <Router>
-          <ScrollToTop/>
-      <Header/>
-      <AppRoutes/>
-
-          <Footer/>
+        <Router>
+          <ScrollToTop />
+          <Header />
+          <AppRoutes />
+          <Footer />
         </Router>
       </body>
     </html>
